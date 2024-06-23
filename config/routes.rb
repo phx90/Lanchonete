0b67686e1d2a7629591116789f5aa1d79de37b99
Pedro Pires <pedro.2007.brasil@hotmail.com>
2024-06-23 20:05:22 -0300

    primeiro commit, inicializando as categories

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
+  devise_for :admins
+
+  authenticated :admin do
+    root to: "admin#index", as: :admin_root
+  end 
+
+  get "admin" => "admin#index"
+   root "home#index"
+   
+end
